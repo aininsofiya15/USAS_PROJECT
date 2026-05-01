fcced6b0d23ec470e32651370ec751fb0b12e2a6
aininsofiya15 <aininzul1502@gmail.com>
2026-05-02 02:09:22 +0800

    k

--- frontend/lib/config/api.dart
+++ frontend/lib/config/api.dart
@@ -3,7 +3,7 @@ class Api {
   // Chrome: http://127.0.0.1:8000/api
   static const String baseUrl = "http://10.0.2.2:8000/api";
 
-  // Auth Endpoints
+  // Auth 
   static const String login = "$baseUrl/login";
 
   // Pusat Adab Module
