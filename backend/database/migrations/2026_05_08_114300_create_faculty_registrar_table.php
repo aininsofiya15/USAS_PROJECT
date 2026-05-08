931dc4262beca511318cb765efdd8e2428c74c59
hdy4hhzn <163952132+hdy4hhzn@users.noreply.github.com>
2026-05-08 19:59:52 +0800

    add

---
+++ backend/database/migrations/2026_05_08_114300_create_faculty_registrar_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('faculty_registrar', function (Blueprint $table) {
+
+            $table->id('registrar_id');
+
+            $table->unsignedBigInteger('id');
+
+            $table->string('department', 100);
+
+            $table->foreign('id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('faculty_registrar');
+    }
+};
