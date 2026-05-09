1296e6e4050c296902cae15a3c030106b6c32aab
wahidahsyarini <cb23024@adab.umpsa.edu.my>
2026-05-09 22:08:42 +0800

    Update DatabaseSeeder.php

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call(LecturerSeeder::class);
         $this->call(SubjectSeeder::class);
         $this->call(SectionSeeder::class);
-          $this->call(BookingSeeder::class);
+        $this->call(LabSeeder::class);
+        $this->call(BookingSeeder::class);
         $this->call(ModuleSeeder::class);
         $this->call(StudentSeeder::class );
         $this->call(AttendanceRecordSeeder::class);
