ff922b8bd8d162f15fe6a35edb0b2174f0600890
hdy4hhzn <163952132+hdy4hhzn@users.noreply.github.com>
2026-06-17 19:38:29 +0800

    uncomment

--- backend/app/Http/Controllers/StudentSubjectController.php
+++ backend/app/Http/Controllers/StudentSubjectController.php
@@ -62,7 +62,7 @@ class StudentSubjectController extends Controller
     public function getRegisteredSubjects($student_id)
     {
         // Sync approved curriculum claims before displaying
-        //$this->syncApprovedCreditClaims($student_id);
+        $this->syncApprovedCreditClaims($student_id);
 
         $registrations = DB::table('registration')
 
